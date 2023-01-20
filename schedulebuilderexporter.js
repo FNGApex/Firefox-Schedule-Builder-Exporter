7fab681467c5f0320b73e27aa21f86c8f2a9021d
Apex <33583602+FNGApex@users.noreply.github.com>
2023-01-19 18:00:55 -0800

    Update schedulebuilderexporter.js

--- schedulebuilderexporter.js
+++ schedulebuilderexporter.js
@@ -1,4 +1,18 @@
 let courses = document.body.getElementsByClassName(
   "CourseItem gray-shadow-border clearfix"
 );
-for(let i = 0; i < courses.length;)
+for (let i = 0; i < courses.length; i++) {
+  let meetingTimes = courses[i].getElementsByClassName("data meetings-times");
+  for (let j = 0; j < meetingTimes.length; j++) {
+    console.log(meetingTimes);
+    if (meetingTimes[j].length == 1) {
+      break;
+    } else {
+      console.log(["Type", meetingTimes.childNodes[0]]);
+      console.log(["Date", meetingTimes.childNodes[1]]);
+      console.log(["Time", meetingTimes.childNodes[2]]);
+      console.log(["Location", meetingTimes.childNodes[3]]);
+    }
+  }
+  console.log("##############");
+}
